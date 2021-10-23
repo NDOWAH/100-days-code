a8dd7def1a6fdba5e784e471a6cc5bbdeec28995
Marcel Grossmann <marcelgrossmann80@gmail.com>
2021-10-23 08:53:58 +0100

    day one of a hundred days of code with Angela

---
+++ E3.py
@@ -0,0 +1,12 @@
+#Write your code below this line 👇
+
+name = input("what is the name please?: ")
+print(len(name))
+
+# In the absence of a len function the code is as below.
+# To test the second code comment the first and uncomment the second starting from count before you run it 
+# count = 0
+# name = list(input("what is the name please?: "))
+# for letter in name:
+#   count +=1
+# print(count)
