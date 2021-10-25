7d98543ed91a07ca06626f25a887cca29129e0d4
Marcel Grossmann <marcelgrossmann80@gmail.com>
2021-10-25 23:37:38 +0100

    day-2 projects of 100 days of code.

---
+++ day2/E2.py
@@ -0,0 +1,10 @@
+# This code calculates the body mass index
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+body_mass_index = weight / height
+
+print(body_mass_index)
