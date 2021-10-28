a77037306f47b20b2fd928092e638b3adafe5afb
Marcel Grossmann <marcelgrossmann80@gmail.com>
2021-10-28 11:00:39 +0100

    day 4 project.

---
+++ day4/project.py
@@ -0,0 +1,52 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+import random
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors."))
+
+computer_choice = random.randint(0,2)
+if user_choice == computer_choice:
+  print("It's a draw game")
+elif user_choice == 0 and computer_choice == 1:
+  print("Sorry, you loose!")
+  print("The computer choose", computer_choice)
+elif user_choice == 0 and computer_choice == 2:
+  print("Congratulation, you won!")
+  print("The computer choose", computer_choice)
+elif user_choice == 1 and computer_choice == 0:
+  print("Sorry, you loose!")
+elif user_choice == 1 and computer_choice == 2:
+  print("Sorry, you loose!")
+elif user_choice == 2 and computer_choice == 0:
+  print("Sorry, you loose")
+elif user_choice == 2 and computer_choice == 1:
+  print("Sorry, you loose")
+elif user_choice == 2 and computer_choice == 1:
+  print("Congratulation, you won!")
+elif user_choice == 1 and computer_choice == 2:
+  print("Sorry, you loose!")
