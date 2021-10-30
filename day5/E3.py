5d2ca5fef9966aac4541ac519cf8e99caf0cd24a
Marcel Grossmann <marcelgrossmann80@gmail.com>
2021-10-30 08:26:13 +0100

    updated

--- day5/E3.py
+++ day5/E3.py
@@ -1,12 +1,12 @@
 #Write your code below this row 👇
 total = 0
-for number in range(1, 101, 2):
+for number in range(2, 101, 2):
   total += number
 print(total)
 
 # Method 2
-total = 0
-for number in range(1,101):
-  if number % 2 == 0:
-      total += number
-print(total)
+# total = 0
+# for number in range(1,101):
+#   if number % 2 == 0:
+#       total += number
+# print(total)
