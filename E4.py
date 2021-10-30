22600f03e60e81285d4db5b3407f9daaaf1cf0f3
Marcel Grossmann <marcelgrossmann80@gmail.com>
2021-10-30 09:55:38 +0100

    Solution to exercise 4 of day 4

---
+++ E4.py
@@ -0,0 +1,25 @@
+# #Write your code below this row 👇
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 ==0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
+# Method 2
+# for number in range(1, 100):
+#   if number % 3 == 0:
+#     if number % 5 == 0:
+#       print("FizzBuzz")
+#     else:
+#         print("Fizz")
+#   elif number % 5 == 0:
+#     if number % 3 == 0:
+#       print("FizzBuzz")
+#     else:
+#       print("Buzz")
+#   else:
+#       print(number)
+
